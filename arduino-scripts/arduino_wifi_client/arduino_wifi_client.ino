fec0d87b7328db219d5049b07d89c2d0c49ee5d2
alessiacorni <cornialessia@gmail.com>
2025-02-12 17:54:03 +0100

    lighter version of the arduino wifi

--- arduino-scripts/arduino_wifi_client/arduino_wifi_client.ino
+++ arduino-scripts/arduino_wifi_client/arduino_wifi_client.ino
@@ -19,7 +19,7 @@
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
-char serverAddress[] = "192.168.0.100"; 
+char serverAddress[] = "192.168.1.58"; //TO BE CHECKED THE DAY OF THE EXAM
 int port = 8080;
 
 long lastGet = 0;
@@ -30,33 +30,27 @@ WiFiManager wifiManager(SECRET_SSID, SECRET_PASS, serverAddress, port);
 LiquidCrystal lcd(RS_PIN, EN_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
 
 std::map<String, String> ids = { //dict for stalls and bathrooms ids
-    {"1.1", "2f79fd90-b162-11ef-aaf6-af873acb8c03"},
-    {"1.2", "0501d6e0-b163-11ef-aaf6-af873acb8c03"},
-    {"2.1", "4e045f10-b164-11ef-aaf6-af873acb8c03"},
-    {"2.2", "58f79040-b164-11ef-aaf6-af873acb8c03"},
-    {"1", "c3ae1950-e561-11ef-a4ea-33e3923212f2"},
-    {"2", "e71e8460-e561-11ef-a4ea-33e3923212f2"}
+    {"1.1", "a29906c0-e7fb-11ef-8c48-2795bb74502b"},//"2f79fd90-b162-11ef-aaf6-af873acb8c03"},
+    {"1.2", "a2802790-e7fb-11ef-8c48-2795bb74502b"},//"0501d6e0-b163-11ef-aaf6-af873acb8c03"}, 
+    {"2.1", "a2a19240-e7fb-11ef-8c48-2795bb74502b"},//"4e045f10-b164-11ef-aaf6-af873acb8c03"},
+    {"2.2", "a2abcb70-e7fb-11ef-8c48-2795bb74502b"},//"58f79040-b164-11ef-aaf6-af873acb8c03"},
+    {"1", "a2b89cb0-e7fb-11ef-8c48-2795bb74502b"},//"c3ae1950-e561-11ef-a4ea-33e3923212f2"},
+    {"2", "a2be9020-e7fb-11ef-8c48-2795bb74502b"}//"e71e8460-e561-11ef-a4ea-33e3923212f2"}
 };
 
-std::map<String, String> responses; //dict for the http responses 
-std::map<String, String> states = { //dict for the stalls state
-    {"1.1", ""},
-    {"1.2", ""},
-    {"2.1", ""},
-    {"2.2", ""}
-};
-std::map<String, String> toilet_papers = { //dict for the stalls toilet paper percentage 
-    {"1.1", ""},
-    {"1.2", ""},
-    {"2.1", ""},
-    {"2.2", ""}
-};
-std::map<String, String> num_people = { //dict for the number of people in queue for each bathroom
-    {"1", ""},
-    {"2", ""}
-};
+String stall_response; //http response for stall
+String bathroom_response; //http response for bathroom
+String stall_state;
+String toilet_paper;
+String num_people;
+String bathroom_state;
+JsonDocument doc;
+String endpoint;
+String authHeader = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiMDQ2YTU2MjAtZTdmYi0xMWVmLWJjMzQtZDE4MThhMzQxZjNiIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiI3MjhmNWNhMS1iZWRjLTQ4ZjUtYjJiYy0wMDhlNGNmMGI5NTMiLCJleHAiOjE3NDAyMzg3NjMsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNzM5Mzc0NzYzLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMDNlN2RmYjAtZTdmYi0xMWVmLWJjMzQtZDE4MThhMzQxZjNiIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.h2bPTkUDNNFMwFAaZucREbJJlgdCAyg-5fwStRYCn2t33ehPezew8wzMS7yJfPyO2rkGiQ0Ace1fVVXmkMxz8w"; //eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiMmI2YWUyMjAtYjE1Yi0xMWVmLWFkM2YtODE1ZWRhNzJiNTg0Iiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiI0MWM3MzAwNS04ZjRhLTQ2YmEtOWM0MS1hOWQ0ZjhlYTNlYWEiLCJleHAiOjE3NDIwMzgxODksImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNzM5MDM4MTkwLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMmIzZjE3MzAtYjE1Yi0xMWVmLWFkM2YtODE1ZWRhNzJiNTg0IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.oWZccfVAiA-54XDCi4Pevd0roJZeEmjKpy3LqAscxPr2VVcdrxaO2gELQH5o9lQeZ9SVg7pyfr7O4Wz--ZCK8w";
 
-void display_info(int potVal, std::map<String, String> num_people, std::map<String, String> states, std::map<String, String> toilet_papers);
+
+void display_info(String num_people, String bathroom_state, String stall_state, String toilet_paper);
+void get_and_deserialize(String bath_id, String stall_id);
 
 void setup() {
   lcd.begin(16, 2);
@@ -66,100 +60,71 @@ void setup() {
 
 void loop() {
   if(millis() - lastGet >= GET_REQUEST_INTERVAL){
-    for (const auto& pair : ids) {
-      String endpoint = "/api/plugins/telemetry/DEVICE/" + pair.second + "/values/timeseries";
-      String authHeader = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiMmI2YWUyMjAtYjE1Yi0xMWVmLWFkM2YtODE1ZWRhNzJiNTg0Iiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiI0MWM3MzAwNS04ZjRhLTQ2YmEtOWM0MS1hOWQ0ZjhlYTNlYWEiLCJleHAiOjE3NDIwMzgxODksImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNzM5MDM4MTkwLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMmIzZjE3MzAtYjE1Yi0xMWVmLWFkM2YtODE1ZWRhNzJiNTg0IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.oWZccfVAiA-54XDCi4Pevd0roJZeEmjKpy3LqAscxPr2VVcdrxaO2gELQH5o9lQeZ9SVg7pyfr7O4Wz--ZCK8w";
-
-      responses[pair.first] = wifiManager.sendGetRequest(endpoint.c_str(), authHeader.c_str());
-    }
-    for (const auto& pair : responses) {
-      Serial.print("Key: ");
-      Serial.print(pair.first);    
-      Serial.print(", Value: ");
-      Serial.println(pair.second); 
-    }
-    lastGet = millis();
-
-    //deserializing the number of people in each bathroom from the JSON
-    for (const auto& pair : num_people){
-      JsonDocument doc;
-      DeserializationError error = deserializeJson(doc, responses[pair.first]);
-      if (error) {
-        Serial.print("Error parsing JSON: ");
-        Serial.println(error.c_str());
-      } else {
-        JsonArray people = doc["People"].as<JsonArray>();
-        num_people[pair.first] = people[0]["value"];
-      }
-    }
-
-    //deserializing the State and the Toilet Paper from the JSON
-    for (const auto& pair : states){
-      JsonDocument doc;
-      DeserializationError error = deserializeJson(doc, responses[pair.first]);
-      if (error) {
-        Serial.print("Error parsing JSON: ");
-        Serial.println(error.c_str());
-      } else {
-        JsonArray state = doc["State"].as<JsonArray>();
-        states[pair.first] = state[0]["value"];
-        JsonArray toilet_paper = doc["ToiletPaper"].as<JsonArray>();
-        toilet_papers[pair.first] = toilet_paper[0]["value"];
-      }
-    }
 
     //reading from the potentiometer pin
     int potVal = analogRead(POT_PIN);
-    printf(potVal);
     Serial.println(potVal);
-    
-    //display bathrooms/stalls info based on potVal
-    display_info(potVal, num_people, states, toilet_papers);
-    lastDisplay = millis();
+
+    if(potVal < 256){
+      get_and_deserialize("1", "1.1");
+      display_info("1B", "1S", num_people, bathroom_state, stall_state, toilet_paper);
+    } 
+    else if(potVal < 512){
+      get_and_deserialize("1", "1.2");
+      display_info("1B", "2S", num_people, bathroom_state, stall_state, toilet_paper);
+    }
+    else if(potVal < 768){
+      get_and_deserialize("2", "2.1");
+      display_info("2B", "1S", num_people, bathroom_state, stall_state, toilet_paper);
+    }
+    else{
+      get_and_deserialize("2", "2.2");
+      display_info("2B", "2S", num_people, bathroom_state, stall_state, toilet_paper);
+    }
+    lastGet = millis();
   }
 }
 
-void display_info(int potVal, std::map<String, String> num_people, std::map<String, String> states, std::map<String, String> toilet_papers){
+void get_and_deserialize(String bath_id, String stall_id){
+  endpoint = "/api/plugins/telemetry/DEVICE/" + ids[bath_id] + "/values/timeseries";
+  bathroom_response = wifiManager.sendGetRequest(endpoint.c_str(), authHeader.c_str());
+
+  endpoint = "/api/plugins/telemetry/DEVICE/" + ids[stall_id] + "/values/timeseries";
+  stall_response = wifiManager.sendGetRequest(endpoint.c_str(), authHeader.c_str());
+
+  DeserializationError error = deserializeJson(doc, bathroom_response);
+  if (error) {
+    Serial.print("Error parsing JSON: ");
+    Serial.println(error.c_str());
+  } else {
+    JsonArray people = doc["People"].as<JsonArray>();
+    num_people = people[0]["value"].as<String>();
+    JsonArray bath_state = doc["State"].as<JsonArray>();
+    bathroom_state = bath_state[0]["value"].as<String>();
+  }
+  //deserializing state and toilet paper
+  error = deserializeJson(doc, stall_response);
+  if (error) {
+    Serial.print("Error parsing JSON: ");
+    Serial.println(error.c_str());
+  } else {
+    JsonArray state = doc["State"].as<JsonArray>();
+    stall_state = state[0]["value"].as<String>();
+    JsonArray paper = doc["ToiletPaper"].as<JsonArray>();
+    toilet_paper = paper[0]["value"].as<String>();
+  }
+}
+
+void display_info(String b_id, String s_id, String num_people, String bathroom_state, String stall_state, String toilet_paper){
   lcd.clear();
 
-  String state;
-  String people_text;
-  String tp_text;
-  String stall_label;
-
-  if(potVal < 256){
-    people_text = "1B people:" + num_people["1"];
-    stall_label = "1S ";
-    tp_text = " tp:" + toilet_papers["1.1"] + "%";
-    state = states["1.1"].substring(0, min(13 - tp_text.length(), states["1.1"].length()));
-  } 
-  else if(potVal < 512){
-    people_text = "1B people:" + num_people["1"];
-    stall_label = "2S ";
-    tp_text = " tp:" + toilet_papers["1.2"] + "%";
-    state = states["1.2"].substring(0, min(13 - tp_text.length(), states["1.2"].length()));
-  }
-  else if(potVal < 768){
-    people_text = "2B people:" + num_people["2"];
-    stall_label = "1S ";
-    tp_text = " tp:" + toilet_papers["2.1"] + "%";
-    state = states["2.1"].substring(0, min(13 - tp_text.length(), states["2.1"].length()));
-  }
-  else{
-    people_text = "2B people:" + num_people["2"];
-    stall_label = "2S ";
-    tp_text = " tp:" + toilet_papers["2.2"] + "%";
-    state = states["2.2"].substring(0, min(13 - tp_text.length(), states["2.2"].length()));
-  }
-
   lcd.setCursor(0, 0);
-  lcd.print(people_text);
+  String people_text = "np:" + num_people;
+  String first_line = b_id + " " + bathroom_state.substring(0, min(12 - people_text.length(), bathroom_state.length())) + " " + people_text;
+  lcd.print(first_line);
 
   lcd.setCursor(0, 1);
-  lcd.print(stall_label);
-  lcd.print(state);
-
-  lcd.setCursor(16 - tp_text.length(), 1);
-  lcd.print(tp_text);
-
+  String tp_text = " tp:" + toilet_paper + "%";
+  String second_line = s_id + " " + stall_state.substring(0, min(13 - tp_text.length(), stall_state.length())) + tp_text;
+  lcd.print(second_line);
 }
